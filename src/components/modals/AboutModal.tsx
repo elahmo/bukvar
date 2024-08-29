ad05af612f0a4e726858f6970904da1015c0492d
elahmo <ahmetnovalic@gmail.com>
2024-08-29 23:13:44 +0200

    fix lint

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -10,22 +10,23 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
     <BaseModal title="O igri" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         Ovo je verzija popularne igre na bosanskom jeziku. Više informacija o
-        igri na {' '}
+        igri na{' '}
         <a
           href="https://novalic.xyz/projects/#bukvar"
           className="underline font-bold"
         >
-        ovoj adresi
-        </a>. Ukoliko želiš pomoći, javi se autoru ili pogledaj izvorni kod {' '}
+          ovoj adresi
+        </a>
+        . Ukoliko želiš pomoći, javi se autoru ili pogledaj izvorni kod{' '}
         <a
           href="https://github.com/elahmo/bukvar"
           className="underline font-bold"
         >
           ovdje
-        </a>.{' '} 
-        <br /><br />
+        </a>
+        . <br />
+        <br />
         Nova riječ svaki dan!
-
       </p>
     </BaseModal>
   )
